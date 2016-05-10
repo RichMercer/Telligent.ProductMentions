2368048aa890e8b4d78706dbb8169bd30e3374d5
RichMercer <rich@richmercer.com>
2016-05-10 12:17:30 +0100

    Initial commit of the mentions sample plugin.

---
+++ Telligent.ProductMentions/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentions
+{
+    public class Constants
+    {
+        public static string ContentTypeName = "Rexel Product";
+
+        public static readonly Guid ApplicationTypeId = new Guid("b039564e3d29490199b4261d48096a5a");
+
+        public static readonly Guid ContentTypeId = new Guid("a426449aa97f41c3ac6699217953ecd1");
+    }
+}
